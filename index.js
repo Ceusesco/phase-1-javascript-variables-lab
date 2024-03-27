03b6f0873549998204a48a059c84e5ddf587ee08
Ceusesco <ceusescon@gmail.com>
2024-03-27 07:26:50 +0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+// let companyName = 'Scuber';
+
+//const
+const companyName='Scuber';
+
+//Second test
+ mostProfitableNeighborhood ='Chelsea';
+ let mostProfitableNeighborhood = 'Chelsea';
+
+
+ //Third test
+ companyCeo ='Susan Smith';
+ let companyCeo ='Susan Smith';
+ 
